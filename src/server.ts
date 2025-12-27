e4f7dc183afe018dfe4acf8ed0970adf0ae93857
Ramon <ramondiiaas@hotmail.com>
2025-12-27 14:24:31 -0300

    chore: adicionar conexão com o banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,28 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import { connectDB } from './database.js';
 
 var app = express();
+dotenv.config();
 
 app.use(cors());
 
 app.use("/", (req: Request, res: Response, next: NextFunction) => {
-    res.send("Hello World");
+  res.send("Hello World");
 })
 
-app.listen(8000, function () {
-  console.log("App listening on port 8000!");
-});
+const startServer = async () => {
+  try {
+    await connectDB();
 
-export default app;
+    app.listen(8000, function () {
+      console.log("App listening on port 8000!");
+    });
+  } catch (error) {
+    console.error('Failed to start server:', error);
+    process.exit(1);
+  }
+};
+
+startServer();
